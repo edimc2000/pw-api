2085e19c9cfe57b092ab5bf840e492270c251e29
Eddie C. <edimc2000@gmail.com>
2025-07-18 23:15:10 -0500

    prep jenkins

---
+++ tests/go-rest.api.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('GO REST API', () => {
+
+    test('Get list of users - TECH GLOBAL', async ({ request }) => {
+        const getUsersList = await request.get(``)
+        // expect reponse 200
+        expect(getUsersList.status()).toEqual(200)
+
+        // debugging
+        // console.log (getUsersList.status())    
+        console.log(await getUsersList.json())
+        console.log((await getUsersList.json())[1])
+    })
+})
